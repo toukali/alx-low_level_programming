8f904f8f7967a2bf86f4980b69b12301848ba9d2
toukali <toukalisamir@outlook.com>
2023-09-13 18:01:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - Prints _putchar as a message. 
- * Return: Always 0 (Success)
- */
+* main - Prints _putchar as a message. 
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -10,11 +10,10 @@ _putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
- _putchar('c');
+_putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
- return (0);
+return (0);
 }
-
