c55d10c5bed695d3995a41c1718652f3fe6679af
toukali <toukalisamir@outlook.com>
2023-12-22 06:14:15 -0800

    NULL instead 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,19 +5,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	if (size <= 0)
-		return (0);
+	  return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
 
-	if (table == 0)
-		return (0);
+	if (table == NULL)
+		return (NULL);
 
 	table->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (table->array == 0)
+	if (table->array == NULL)
 	  {
 		free(table);
-		return (0);
+		return (NULL);
 	}
 
 	table->size = size;
